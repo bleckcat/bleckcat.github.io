f0a8e67e7c40e2c02182843b9c9b6faa90c328b2
bleckcat <juanvitor16@hotmail.com>
2023-07-19 22:06:35 +0100

    Making a capsule on top of a cube

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,7 @@ const canvas = document.querySelector(".webgl");
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
@@ -15,23 +15,26 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(20, 20, 0.1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 
+const capsuleGeometry = new THREE.CapsuleGeometry(1, 1, 4, 8);
+const capsuleMaterial = new THREE.MeshBasicMaterial({ color: 0xfff });
+const capsule = new THREE.Mesh(capsuleGeometry, capsuleMaterial);
+
+camera.position.z = 8;
+camera.position.y = -16;
+
+capsule.rotation.x = Math.PI / 2;
+capsule.position.z = 2;
+
+camera.rotation.x = 1;
+
 scene.add(cube);
+scene.add(capsule);
 
-camera.position.z = 5;
-
-function animate() {
-  requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
-}
-
-animate();
+renderer.render(scene, camera);
