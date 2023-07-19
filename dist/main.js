628a078e9c474713b56e3eb4d8b5c4215ed40762
bleckcat <juanvitor16@hotmail.com>
2023-07-19 21:10:44 +0100

    Remove all code that is not usefull

--- dist/main.js
+++ dist/main.js
@@ -13,12 +13,14 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
+
 scene.add(cube);
 
 camera.position.z = 5;
