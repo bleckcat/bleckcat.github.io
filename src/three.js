ef637b36608523d1d1149ac212d4fea3c08ec9f4
bleckcat <juanvitor16@hotmail.com>
2023-08-11 17:21:57 +0100

    changed cat model and now it walks

--- src/three.js
+++ src/three.js
@@ -1,21 +1,28 @@
 import * as THREE from "three";
-import createGroundPlane from "./components/ground";
+import createGroundPlane from "./utils/ground";
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { CharacterControls } from "./utils/characterControls";
+
+// MODELS
 import backgroundBall from "/assets/fantasy_sky_background/scene.gltf";
+import toonCat from "/assets/toon_cat/scene.gltf";
 
 function renderThreeJs(element) {
+  // CANVAS
   const canvas = element;
-
+  // SCENE
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(0xabcdef);
-
+  // RENDERER
   const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     depthBuffer: true,
   });
-
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  // CAMERA
   const camera = new THREE.PerspectiveCamera(
     50,
     window.innerWidth / window.innerHeight,
@@ -31,15 +38,13 @@ function renderThreeJs(element) {
   orbitControls.maxPolarAngle = Math.PI / 2 - 0.05;
   orbitControls.update();
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.shadowMap.enabled = true;
-
-  const ambientLight = new THREE.AmbientLight(0xadd8e6, 3);
-  createGroundPlane(scene);
-
+  // CONSTS
   const loader = new GLTFLoader();
+  const clock = new THREE.Clock();
+  const ambientLight = new THREE.AmbientLight(0xadd8e6, 3);
 
-  loader.load(backgroundBall, (gltf) => {
+  //SKY
+  loader.load(backgroundBall, function (gltf) {
     const model = gltf.scene;
     scene.add(model);
 
@@ -60,12 +65,66 @@ function renderThreeJs(element) {
     model.scale.set(13, 13, 13);
   });
 
-  const clock = new THREE.Clock();
+  loader.load(toonCat, function (gltf) {
+    const model = gltf.scene;
+    model.traverse(function (object) {
+      if (object.isMesh) object.castShadow = true;
+    });
+    scene.add(model);
+
+    const gltfAnimations = gltf.animations;
+    console.log(gltfAnimations);
+    const mixer = new THREE.AnimationMixer(model);
+    const animationsMap = new Map();
+    gltfAnimations
+      .filter((a) => a.name != "TPose")
+      .forEach((a) => {
+        animationsMap.set(a.name, mixer.clipAction(a));
+      });
+
+    characterControls = new CharacterControls(
+      model,
+      mixer,
+      animationsMap,
+      orbitControls,
+      camera,
+      "Idle"
+    );
+
+    model.scale.set(0.001, 0.001, 0.001);
+  });
+
+  createGroundPlane(scene);
+
+  var characterControls;
+
+  // CONTROL KEYS
+  const keysPressed = {};
+  document.addEventListener(
+    "keydown",
+    (event) => {
+      if (event.shiftKey && characterControls) {
+        characterControls.switchRunToggle();
+      } else {
+        keysPressed[event.key.toLowerCase()] = true;
+      }
+    },
+    false
+  );
+  document.addEventListener(
+    "keyup",
+    (event) => {
+      keysPressed[event.key.toLowerCase()] = false;
+    },
+    false
+  );
 
   // ANIMATE
   function animate() {
     let mixerUpdateDelta = clock.getDelta();
-
+    if (characterControls) {
+      characterControls.update(mixerUpdateDelta, keysPressed);
+    }
     orbitControls.update();
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
@@ -74,6 +133,14 @@ function renderThreeJs(element) {
   animate();
 
   scene.add(ambientLight);
+
+  // RESIZE HANDLER
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+  window.addEventListener("resize", onWindowResize);
 }
 
 export default renderThreeJs;
--- src/utils/ground.js
+++ src/utils/ground.js
@@ -1,7 +1,7 @@
 import { BoxGeometry, Mesh, MeshStandardMaterial } from "three";
 
 function createGroundPlane(scene) {
-  const geometry = new BoxGeometry(10, 0.5, 10);
+  const geometry = new BoxGeometry(2, 0.1, 2);
   const material = new MeshStandardMaterial({ color: 0xfff });
   const plane = new Mesh(geometry, material);
 
